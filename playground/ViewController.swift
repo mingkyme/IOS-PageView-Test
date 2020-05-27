08af0eafc7081d05bf098cff5450402df58bc610
Mingky <mingkyme@gmail.com>
2020-05-27 13:01:50 +0900

    Initial Commit

---
+++ playground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  playground
+//
+//  Created by 김민기 on 2020/05/27.
+//  Copyright © 2020 Mingky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
