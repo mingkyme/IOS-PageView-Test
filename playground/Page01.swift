68d63a3fe855c5cc2b943106309371b400f500ac
Mingky <mingkyme@gmail.com>
2020-06-02 11:15:48 +0900

    페이지 표시기의 블랙 바 제거

---
+++ playground/Page01.swift
@@ -0,0 +1,30 @@
+//
+//  Page01.swift
+//  playground
+//
+//  Created by 김민기 on 2020/05/29.
+//  Copyright © 2020 Mingky. All rights reserved.
+//
+
+import UIKit
+
+class Page01: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- playground/PageViewController.swift
+++ playground/PageViewController.swift
@@ -9,50 +9,64 @@
 import UIKit
 
 class PageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
-    var views:[String] = ["page01","page02","page03","page04"]
-    var index:Int = 0
-    var maxIndex:Int = 3
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        print("down")
-        if index <= 0{
-            return nil
-        }else{
-            
-        
-        index -= 1
-        return getViewControllerAtIndex(index: index)
-        }
-    }
-    
-    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        print("up")
-        if index >= maxIndex{
-            return nil
-        }else{
-        index += 1
-        return getViewControllerAtIndex(index: index)
-}
-        
-    }
-    func getViewControllerAtIndex(index:Int) -> UIViewController{
-        print(index)
-        return self.storyboard!.instantiateViewController(identifier: views[index])
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        dataSource = self
-        delegate = self
-        self.setViewControllers([getViewControllerAtIndex(index: 0)] , direction: UIPageViewController.NavigationDirection.forward, animated: false, completion: nil)
-
-    }
+    var index : Int = 0
+    var identifiers:[String] = ["page01", "page02", "page03"]
+    lazy var pages : [UIViewController] =
+     [
+        GetPageByIndex(index: 0),
+        GetPageByIndex(index: 1),
+        GetPageByIndex(index: 2)
+    ]
     func presentationCount(for pageViewController: UIPageViewController) -> Int {
-
-        return self.views.count
-        //return self.views.count
+        return identifiers.count
     }
-    
-    // 인디케이터 초기 선택 값
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let curPageIndex = pages.firstIndex(of: viewController) else {return nil}
+        let prePageIndex = curPageIndex - 1
+        
+        guard prePageIndex >= 0 else { return nil }
+        guard pages.count > prePageIndex else { return nil }
+        return GetPageByIndex(index: prePageIndex)
+        
+    }
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = pages.firstIndex(of: viewController) else { return nil }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex < pages.count else { return nil }
+        
+        guard pages.count > nextIndex else { return nil }
+        
+        return pages[nextIndex]
+    }
+    func GetPageByIndex(index:Int)-> UIViewController{
+        return self.storyboard!.instantiateViewController(identifier: identifiers[index])
+
+    }
     func presentationIndex(for pageViewController: UIPageViewController) -> Int {
         return 0
+    }
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.delegate = self
+        self.dataSource = self
+        
+        self.setViewControllers([pages[0]] , direction: UIPageViewController.NavigationDirection.forward, animated: false, completion: nil)
+
+        let appearance = UIPageControl.appearance(whenContainedInInstancesOf: [UIPageViewController.self])
+        appearance.pageIndicatorTintColor = UIColor.white
+        appearance.currentPageIndicatorTintColor = UIColor.red
+    }
+    override func viewDidLayoutSubviews() {
+        //corrects scrollview frame to allow for full-screen view controller pages
+        for subView in self.view.subviews {
+            if subView is UIScrollView {
+                subView.frame = self.view.bounds
+            }
+        }
+        super.viewDidLayoutSubviews()
     }
 }
