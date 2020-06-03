8f332e380f249f536200b226e15c46329129713e
Mingky <mingkyme@gmail.com>
2020-06-03 13:07:19 +0900

    migration

---
+++ playground/DoubleTap/DoubleTapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  DoubleTapViewController.swift
+//  playground
+//
+//  Created by 김민기 on 2020/06/02.
+//  Copyright © 2020 Mingky. All rights reserved.
+//
+
+import UIKit
+
+class DoubleTapViewController: UIViewController {
+
+//    @IBOutlet weak var v2: UIView!
+//
+//    @IBOutlet weak var vh: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        let t2 = UITapGestureRecognizer()
+//        t2.numberOfTapsRequired = 2
+//        t2.addTarget(self, action: #selector(doubleTapped))
+//        v2.addGestureRecognizer(t2)
+    }
+//    @objc func doubleTapped(sender: UITapGestureRecognizer) {
+//        print(sender.view == v2)
+//        UIView.animate(withDuration: 0.5, animations: {
+//            self.vh.alpha = 1
+//        },completion:{finished in
+//                self.vh.alpha = 0
+//            })
+//    }
+}
